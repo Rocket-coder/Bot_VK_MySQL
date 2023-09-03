bf428a1055f54798b4af0791adee2732b1d41837
Rocket <komarovo203@gmail.com>
2023-09-03 16:17:52 +0300

    Second

--- plugins/system.py
+++ plugins/system.py
@@ -5,8 +5,6 @@ import plugins.mysql as SQL
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
-
-
 image = "photo-220949620_457239018"
 
 
--- start.py
+++ start.py
@@ -1,8 +1,3 @@
-import vk_api
 import plugins.system as sys
-import plugins.text as tx
-from vk_api.longpoll import VkLongPoll, VkEventType
-
-
 
 sys.start()
